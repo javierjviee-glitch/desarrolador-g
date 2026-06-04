27ad6b60791b17cbe4783c3e9699b59d28508aca
javier <javier@gmail.com>
2026-06-05 00:30:12 +0200

    Inicializo la entidad, asigno una casa aleatoria al usar el sombrero y deberia sonar un sonido añadido

--- plugins/sombrero/entities/entities/ix_sombrero/init.lua
+++ plugins/sombrero/entities/entities/ix_sombrero/init.lua
@@ -10,3 +10,21 @@ local casas = {
     "Ravenclaw",
     "Hufflepuff"
 }
+function ENT:Initialize()
+    self:SetModel("models/genos/sortinghat.mdl")
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+end
+
+function ENT:Use(activator)
+    if not IsValid(activator) or not activator:IsPlayer() then return end
+
+    local casa = casas[math.random(#casas)]
+
+    net.Start("SombreroSeleccionado")
+    net.WriteString(casa)
+    net.Send(activator)
+
+    sound.Play("genosrp/" .. string.lower(casa) .. ".wav", self:GetPos(), 75, 100, 1)
+end
