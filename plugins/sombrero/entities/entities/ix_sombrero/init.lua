e81ce401fffe68055a45a6913a6b92ea4e506d11
javier <javier@gmail.com>
2026-06-04 23:00:57 +0200

    Añado los archivos cliente/shared, y  registro el mensaje definiendo las casas

---
+++ plugins/sombrero/entities/entities/ix_sombrero/init.lua
@@ -0,0 +1,12 @@
+AddCSLuaFile("shared.lua")
+AddCSLuaFile("cl_init.lua")
+include("shared.lua")
+
+util.AddNetworkString("SombreroSeleccionado")
+
+local casas = {
+    "Gryffindor",
+    "Slytherin",
+    "Ravenclaw",
+    "Hufflepuff"
+}
