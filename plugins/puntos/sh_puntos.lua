e166af03f5b21ceb8e654e57be31926128a72161
javier <javier@gmail.com>
2026-06-05 17:07:57 +0200

    Creamos el menú de puntos y envío los cambios de los puntos de casa al servidor

--- plugins/puntos/sh_puntos.lua
+++ plugins/puntos/sh_puntos.lua
@@ -24,3 +24,52 @@ if SERVER then
         net.Send(ply)
     end)
 end
+
+if CLIENT then
+    net.Receive("AbrirMenuPuntos", function()
+        local puntos = net.ReadTable()
+
+        local frame = vgui.Create("DFrame")
+        frame:SetSize(300, 400)
+        frame:Center()
+        frame:SetTitle("Puntos de las Casas")
+        frame:MakePopup()
+
+        local casas = {
+            {nombre = "Gryffindor", color = Color(200, 0, 0)},
+            {nombre = "Slytherin", color = Color(0, 150, 0)},
+            {nombre = "Ravenclaw", color = Color(0, 100, 200)},
+            {nombre = "Hufflepuff", color = Color(200, 200, 0)}
+        }
+
+        for _, casa in ipairs(casas) do
+            local panel = vgui.Create("DPanel", frame)
+            panel:Dock(TOP)
+            panel:SetTall(50)
+
+            panel.Paint = function(self, w, h)
+                draw.RoundedBox(4, 0, 0, w, h, casa.color)
+                draw.SimpleText(casa.nombre .. ": " .. puntos[casa.nombre], "DermaLarge", 10, 10, color_white)
+            end
+             local add = vgui.Create("DButton", panel)
+            add:Dock(RIGHT)
+            add:SetText("+")
+            add.DoClick = function()
+                net.Start("ActualizarPuntos")
+                net.WriteString(casa.nombre)
+                net.WriteInt(1, 16)
+                net.SendToServer()
+            end
+
+            local sub = vgui.Create("DButton", panel)
+            sub:Dock(RIGHT)
+            sub:SetText("-")
+            sub.DoClick = function()
+                net.Start("ActualizarPuntos")
+                net.WriteString(casa.nombre)
+                net.WriteInt(-1, 16)
+                net.SendToServer()
+            end
+        end
+    end)
+end
