fc0d182184e2a0a06cdac908b5295d16b470a120
javier <javier@gmail.com>
2026-06-06 14:15:47 +0200

    Correccion en el sistema de puntos y añadido del mismo

--- plugins/puntos/sh_plugin.lua
+++ plugins/puntos/sh_plugin.lua
@@ -1,3 +1,9 @@
 PLUGIN.name = "Puntos Casas"
 PLUGIN.author = "Javi"
 PLUGIN.description = "Sistema de puntos de Hogwarts."
+
+ix.util.Include("sh_puntos.lua")
+
+ix.config.Add("permitirModificarPuntos", true, "Permite sumar/restar puntos.", nil, {
+    category = "Puntos"
+})
